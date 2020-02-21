5183691a2e7edd8ece0ab78629ef4aed4f9cc037
kevinryall <kevin.ryall@estafet.com>
2020-02-21 09:59:34 +0000

    Add files via upload

---
+++ Repo/repo-load-sonarqube-images.sh
@@ -0,0 +1,8 @@
+docker pull docker.io/sonarqube:7.9-community
+docker pull docker.io/postgres:9.5
+
+docker tag docker.io/sonarqube:7.9-community repo.thales.com:5000/sonarqube:7.9-community
+docker tag docker.io/postgres:9.5 repo.thales.com:5000/strimzi/postgres:9.5
+
+docker push repo.thales.com:5000/sonarqube:7.9-community
+docker push repo.thales.com:5000/postgres:9.5
