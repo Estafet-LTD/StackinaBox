7a7f1988db6ad79901a8f2a8eb6273498f34082d
kevinryall <kevin.ryall@estafet.com>
2020-02-24 11:15:17 +0000

    downgrade sonarqube to 7.4

--- OCP/pods/load-sonarqube-images.sh
+++ OCP/pods/load-sonarqube-images.sh
@@ -7,13 +7,13 @@ TOKEN=$(oc whoami -t)
 docker login -u developer -p $TOKEN docker-registry.default.svc.cluster.local:5000
 
 
-docker pull repo.thales.com:5000/sonarqube:7.9-community
+docker pull repo.thales.com:5000/sonarqube:7.4-community
 docker pull repo.thales.com:5000/postgres:9.5
 
-docker tag repo.thales.com:5000/sonarqube:7.9-community docker-registry.default.svc.cluster.local:5000/openshift/sonarqube:7.9-community
+docker tag repo.thales.com:5000/sonarqube:7.9-community docker-registry.default.svc.cluster.local:5000/openshift/sonarqube:7.4-community
 docker tag repo.thales.com:5000/postgres:9.5 docker-registry.default.svc.cluster.local:5000/openshift/postgres:9.5
 
-docker push docker-registry.default.svc.cluster.local:5000/openshift/sonarqube:7.9-community
+docker push docker-registry.default.svc.cluster.local:5000/openshift/sonarqube:7.4-community
 docker push docker-registry.default.svc.cluster.local:5000/openshift/postgres:9.5
 
 oc login -u system:admin
