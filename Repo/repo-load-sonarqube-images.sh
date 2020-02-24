eb3f396629588715a29e4fcfe0100de08aef4912
kevinryall <kevin.ryall@estafet.com>
2020-02-24 13:39:07 +0000

    amending postgresql image

--- Repo/repo-load-sonarqube-images.sh
+++ Repo/repo-load-sonarqube-images.sh
@@ -1,8 +1,8 @@
 docker pull docker.io/sonarqube:7.4-community
-docker pull docker.io/postgres:9.5
+docker pull registry.redhat.io/rhscl/postgresql-95-rhel7
 
 docker tag docker.io/sonarqube:7.4-community repo.thales.com:5000/sonarqube:7.4-community
-docker tag docker.io/postgres:9.5 repo.thales.com:5000/strimzi/postgres:9.5
+docker tag registry.redhat.io/rhscl/postgresql-95-rhel7 repo.thales.com:5000/rhscl/postgresql-95-rhel7
 
 docker push repo.thales.com:5000/sonarqube:7.4-community
-docker push repo.thales.com:5000/postgres:9.5
+docker push repo.thales.com:5000/rhscl/postgresql-95-rhel7
