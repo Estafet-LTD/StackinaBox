77e5f23c9fb9966f3c595b0d45a5296281331261
kevinryall <kevin.ryall@estafet.com>
2020-02-24 13:41:46 +0000

    amending postgresql image

--- OCP/pods/load-sonarqube-images.sh
+++ OCP/pods/load-sonarqube-images.sh
@@ -8,12 +8,12 @@ docker login -u developer -p $TOKEN docker-registry.default.svc.cluster.local:50
 
 
 docker pull repo.thales.com:5000/sonarqube:7.4-community
-docker pull repo.thales.com:5000/postgres:9.5
+docker pull repo.thales.com:5000/rhscl/postgresql-95-rhel7
 
 docker tag repo.thales.com:5000/sonarqube:7.9-community docker-registry.default.svc.cluster.local:5000/openshift/sonarqube:7.4-community
-docker tag repo.thales.com:5000/postgres:9.5 docker-registry.default.svc.cluster.local:5000/openshift/postgres:9.5
+docker tag repo.thales.com:5000/rhscl/postgresql-95-rhel7 docker-registry.default.svc.cluster.local:5000/openshift/postgresql-95-rhel7
 
 docker push docker-registry.default.svc.cluster.local:5000/openshift/sonarqube:7.4-community
-docker push docker-registry.default.svc.cluster.local:5000/openshift/postgres:9.5
+docker push  docker-registry.default.svc.cluster.local:5000/openshift/postgresql-95-rhel7
 
 oc login -u system:admin
