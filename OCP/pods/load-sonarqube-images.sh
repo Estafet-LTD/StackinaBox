5b4b4f79fb63140f92522cbcfd53b70266634e1c
kevinryall <kevin.ryall@estafet.com>
2020-02-25 13:53:47 +0000

    Update load-sonarqube-images.sh

--- OCP/pods/load-sonarqube-images.sh
+++ OCP/pods/load-sonarqube-images.sh
@@ -10,7 +10,7 @@ docker login -u developer -p $TOKEN docker-registry.default.svc.cluster.local:50
 docker pull repo.thales.com:5000/sonarqube:7.4-community
 docker pull repo.thales.com:5000/rhscl/postgresql-95-rhel7
 
-docker tag repo.thales.com:5000/sonarqube:7.9-community docker-registry.default.svc.cluster.local:5000/openshift/sonarqube:7.4-community
+docker tag repo.thales.com:5000/sonarqube:7.4-community docker-registry.default.svc.cluster.local:5000/openshift/sonarqube:7.4-community
 docker tag repo.thales.com:5000/rhscl/postgresql-95-rhel7 docker-registry.default.svc.cluster.local:5000/openshift/postgresql-95-rhel7
 
 docker push docker-registry.default.svc.cluster.local:5000/openshift/sonarqube:7.4-community
