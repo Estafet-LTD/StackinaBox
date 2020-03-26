e1edcc3035b132d66605deb24b8f44cc826e91b2
kevin <kevin@LAPTOP-B796RTUL>
2020-03-26 17:21:01 +0000

    amending to include clair-scanner

--- OCP/pods/load-clair-images.sh
+++ OCP/pods/load-clair-images.sh
@@ -9,11 +9,14 @@ docker login -u developer -p $TOKEN docker-registry-default.apps.ocp.thales.com
 
 docker pull repo.thales.com:5000/clair-local-scan:v2.1.0
 docker pull repo.thales.com:5000/clair-db:2020-03-19 # or whatever you have
+docker pull repo.thales.com:5000/clair-scanner:v1.0
 
 docker tag repo.thales.com:5000/clair-local-scan:v2.1.0 docker-registry-default.apps.ocp.thales.com/openshift/clair-local-scan:v2.1.0
 docker tag repo.thales.com:5000/clair-db:2020-03-19 docker-registry-default.apps.ocp.thales.com/openshift/clair-db:2020-03-19
+docker tag repo.thales.com:5000/clair-scanner:v1.0 docker-registry-default.apps.ocp.thales.com/openshift/clair-scanner:v1.0
 
 docker push docker-registry-default.apps.ocp.thales.com/openshift/clair-local-scan:v2.1.0
 docker push docker-registry-default.apps.ocp.thales.com/openshift/clair-db:2020-03-19
+docker push docker-registry-default.apps.ocp.thales.com/openshift/clair-scanner:v1.0
 
 oc login -u system:admin
